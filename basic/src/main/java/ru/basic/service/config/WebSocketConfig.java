66cfc53c63b0ea39c49abbea9f24df467187d380
Valentin Kudinov <moysait1984@yandex.ru>
2025-03-17 16:31:19 +0400

    Websocket push client update

--- basic/src/main/java/ru/basic/service/config/WebSocketConfig.java
+++ basic/src/main/java/ru/basic/service/config/WebSocketConfig.java
@@ -2,10 +2,12 @@ package ru.basic.service.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@EnableScheduling
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
--- basic/src/main/java/ru/basic/service/websocket/WebSocketController.java
+++ basic/src/main/java/ru/basic/service/websocket/WebSocketController.java
@@ -1,18 +1,52 @@
 package ru.basic.service.websocket;
 
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class WebSocketController {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @Scheduled(fixedRate = 1000)
+    private void sendChange() {
+        log.info("Sending message");
+        simpMessagingTemplate.convertAndSend("/topic/greetings", "{ \"response\": \"Hello, " + "UPDATE" + "!\"}");
+    }
 
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public String greeting(String message) throws Exception {
+    public String greeting(SimpMessageHeaderAccessor headers, String body) throws Exception {
+        // Получаем команду
+        String command = headers.getMessageType().toString();
+
+        // Получаем заголовки
+        MessageHeaders messageHeaders = headers.getMessageHeaders();
+        for (Map.Entry<String, Object> entry : messageHeaders.entrySet()) {
+            System.out.println("Header Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
+        // Получаем тело сообщения
+        System.out.println("Body: " + body);
         // Обработка сообщения, можно добавить задержку для имитации обработки
         Thread.sleep(1000);
-        return "{ \"response\": \"Hello, " + message + "!\"}";
+        return "{ \"response\": \"Hello, " + body + "!\"}";
     }
+
+
 }
